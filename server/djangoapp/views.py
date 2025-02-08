810c3d6a47c0df696426813e3ba9586d463a5062
harisrahim5011 <harisrahim5011@gmail.com>
2025-02-08 08:02:47 -0500

    linting, views.py 3rd

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -10,6 +10,7 @@ from .populate import initiate
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
+
 # Create a `login_request` view to handle sign in request
 def login_user(request):
     data = json.loads(request.body)
@@ -23,10 +24,12 @@ def login_user(request):
         response_data["status"] = "Authenticated"
     return JsonResponse(response_data)
 
+
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
     logout(request)
     return JsonResponse({"userName": ""})
+
 
 # Create a `registration` view to handle sign up request
 def registration(request):
@@ -36,41 +39,61 @@ def registration(request):
     first_name = data['firstName']
     last_name = data['lastName']
     email = data['email']
-    
+
     try:
         User.objects.get(username=username)
-        return JsonResponse({"userName": username, "error": "Already Registered"})
+        return JsonResponse(
+            {"userName": username, "error": "Already Registered"}
+        )
     except User.DoesNotExist:
-        user = User.objects.create_user(username=username, first_name=first_name,
-                                        last_name=last_name, password=password, email=email)
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email
+        )
         login(request, user)
-        return JsonResponse({"userName": username, "status": "Authenticated"})
+        return JsonResponse(
+            {"userName": username, "status": "Authenticated"}
+        )
+
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
     if count == 0:
         initiate()
     car_models = CarModel.objects.select_related('car_make')
-    cars = [{"CarModel": car_model.name, "CarMake": car_model.car_make.name}
-            for car_model in car_models]
+    cars = [
+        {"CarModel": car_model.name, "CarMake": car_model.car_make.name}
+        for car_model in car_models
+    ]
     return JsonResponse({"CarModels": cars})
 
-# Update the `get_dealerships` render list of dealerships, all by default, 
-# particular state if state is passed
+
+# Update the `get_dealerships` render list of dealerships
 def get_dealerships(request, state="All"):
-    endpoint = "/fetchDealers" if state == "All" else f"/fetchDealers/{state}"
+    endpoint = (
+        "/fetchDealers"
+        if state == "All"
+        else f"/fetchDealers/{state}"
+    )
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
+
 
 def get_dealer_reviews(request, dealer_id):
     if dealer_id:
         endpoint = f"/fetchReviews/dealer/{dealer_id}"
         reviews = get_request(endpoint)
         for review_detail in reviews:
-            sentiment = analyze_review_sentiments(review_detail['review'])
+            sentiment = analyze_review_sentiments(
+                review_detail['review']
+            )
             review_detail['sentiment'] = sentiment['sentiment']
         return JsonResponse({"status": 200, "reviews": reviews})
     return JsonResponse({"status": 400, "message": "Bad Request"})
+
 
 def get_dealer_details(request, dealer_id):
     if dealer_id:
@@ -79,6 +102,7 @@ def get_dealer_details(request, dealer_id):
         return JsonResponse({"status": 200, "dealer": dealership})
     return JsonResponse({"status": 400, "message": "Bad Request"})
 
+
 def add_review(request):
     if not request.user.is_anonymous:
         data = json.loads(request.body)
@@ -86,6 +110,10 @@ def add_review(request):
             post_review(data)
             return JsonResponse({"status": 200})
         except Exception as e:
-            logger.error(f"Error in posting review: {e}")
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            logger.error(
+                f"Error in posting review: {e}"
+            )
+            return JsonResponse(
+                {"status": 401, "message": "Error in posting review"}
+            )
     return JsonResponse({"status": 403, "message": "Unauthorized"})
