83ea222eff749b8782992e8e202144962e6315f9
harisrahim5011 <harisrahim5011@gmail.com>
2025-02-08 11:15:43 -0500

    changes made to run mongo-db. up and running

--- server/database/app.js
+++ server/database/app.js
@@ -1,42 +1,46 @@
-import express, { raw } from 'express';
-import { connect } from 'mongoose';
-import { readFileSync } from 'fs';
-import cors from 'cors';
-const app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const cors = require('cors')
+const app = express()
 const port = 3030;
 
-app.use(cors());
+app.use(cors())
 app.use(require('body-parser').urlencoded({ extended: false }));
 
-const reviews_data = JSON.parse(readFileSync("reviews.json", 'utf8'));
-const dealerships_data = JSON.parse(readFileSync("dealerships.json", 'utf8'));
+const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
+const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
-connect("mongodb://mongo_db:27017/", { dbName: 'dealershipsDB' });
+mongoose.connect("mongodb://mongo_db:27017/", { 'dbName': 'dealershipsDB' });
 
-import Reviews, { deleteMany, insertMany, find } from './review';
-import { deleteMany as _deleteMany, insertMany as _insertMany, find as _find } from './dealership';
-import dealership from './dealership';
+
+const Reviews = require('./review');
+
+const Dealerships = require('./dealership');
+const dealership = require('./dealership');
 
 try {
-    deleteMany({}).then(() => {
-        insertMany(reviews_data.reviews); // Changed from ['reviews'] to .reviews
+    Reviews.deleteMany({}).then(() => {
+        Reviews.insertMany(reviews_data['reviews']);
     });
-    _deleteMany({}).then(() => {
-        _insertMany(dealerships_data.dealerships); // Changed from ['dealerships'] to .dealerships
+    Dealerships.deleteMany({}).then(() => {
+        Dealerships.insertMany(dealerships_data['dealerships']);
     });
+
 } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
 }
 
+
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API");
+    res.send("Welcome to the Mongoose API")
 });
 
 // Express route to fetch all reviews
 app.get('/fetchReviews', async (req, res) => {
     try {
-        const documents = await find();
+        const documents = await Reviews.find();
         res.json(documents);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
@@ -46,7 +50,7 @@ app.get('/fetchReviews', async (req, res) => {
 // Express route to fetch reviews by a particular dealer
 app.get('/fetchReviews/dealer/:id', async (req, res) => {
     try {
-        const documents = await find({ dealership: req.params.id });
+        const documents = await Reviews.find({ dealership: req.params.id });
         res.json(documents);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
@@ -55,8 +59,9 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
+    //Write your code here
     try {
-        const documents = await _find();
+        const documents = await Dealerships.find();
         res.json(documents);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
@@ -65,8 +70,9 @@ app.get('/fetchDealers', async (req, res) => {
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
+    //Write your code here
     try {
-        const documents = await _find({ state: req.params.state });
+        const documents = await Dealerships.find({ state: req.params.state });
         res.json(documents);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
@@ -75,30 +81,31 @@ app.get('/fetchDealers/:state', async (req, res) => {
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
+    //Write your code here
     try {
-        const documents = await _find({ id: req.params.id });
+        const documents = await Dealerships.find({ id: req.params.id });
         res.json(documents);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching documents' });
     }
 });
 
-// Express route to insert review
-app.post('/insert_review', raw({ type: '*/*' }), async (req, res) => {
-    const data = JSON.parse(req.body);
-    const documents = await find().sort({ id: -1 });
-    let new_id = documents[0].id + 1; // Changed from ['id'] to .id
+//Express route to insert review
+app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
+    data = JSON.parse(req.body);
+    const documents = await Reviews.find().sort({ id: -1 })
+    let new_id = documents[0]['id'] + 1
 
     const review = new Reviews({
-        id: new_id,
-        name: data.name, // Changed from ['name'] to .name
-        dealership: data.dealership, // Changed from ['dealership'] to .dealership
-        review: data.review, // Changed from ['review'] to .review
-        purchase: data.purchase, // Changed from ['purchase'] to .purchase
-        purchase_date: data.purchase_date, // Changed from ['purchase_date'] to .purchase_date
-        car_make: data.car_make, // Changed from ['car_make'] to .car_make
-        car_model: data.car_model, // Changed from ['car_model'] to .car_model
-        car_year: data.car_year, // Changed from ['car_year'] to .car_year
+        "id": new_id,
+        "name": data['name'],
+        "dealership": data['dealership'],
+        "review": data['review'],
+        "purchase": data['purchase'],
+        "purchase_date": data['purchase_date'],
+        "car_make": data['car_make'],
+        "car_model": data['car_model'],
+        "car_year": data['car_year'],
     });
 
     try {
@@ -114,3 +121,4 @@ app.post('/insert_review', raw({ type: '*/*' }), async (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+// check the test
