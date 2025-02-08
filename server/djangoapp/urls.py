64e70f0cce6ec9f4954780dc966c5b1e861efb92
harisrahim5011 <harisrahim5011@gmail.com>
2025-02-08 07:39:01 -0500

    linting, urls.py 2nd

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,9 +16,11 @@ urlpatterns = [
 
     # path for dealer reviews view
     path('get_dealers', views.get_dealerships, name='get_dealers'),
-    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
+    path('get_dealers/<str:state>', views.get_dealerships,
+         name='get_dealers_by_state'),
     path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
-    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_reviews'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews,
+         name='dealer_reviews'),
 
     # path for add a review view
     path('add_review', views.add_review, name='add_review'),
